a27e60f4e34409566782385846a81fb3ed8b3559
Madhan-Kumar <madhanrkv10@gmail.com>
2023-07-31 23:48:02 +0530

    Added Native TextButton and ElevatedButton

--- lib/src/native_elevated_button.dart
+++ lib/src/native_elevated_button.dart
@@ -2,13 +2,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:native_widget/src/widgets/native_show_widget.dart';
 
-class NativeTextButton extends StatelessWidget {
-  const NativeTextButton(
+class NativeElevatedButton extends StatelessWidget {
+  const NativeElevatedButton(
       {Key? key,
       required this.onPressed,
       required this.child,
       this.padding,
       this.alignment = Alignment.center,
+      this.backgroundColor,
       this.disabledColor = CupertinoColors.quaternarySystemFill,
       this.borderRadius = const BorderRadius.all(Radius.circular(8.0))})
       : super(key: key);
@@ -18,37 +19,54 @@ class NativeTextButton extends StatelessWidget {
   final AlignmentGeometry? alignment;
   final EdgeInsetsGeometry? padding;
   final Color? disabledColor;
+
+  /// Defaults to the [CupertinoTheme] 's `primaryColor` when the background color is Null;
+  /// Defaults to the [MaterialTheme] 's `primaryColor` when the background color is Null;
+
+  final Color? backgroundColor;
   @override
   Widget build(BuildContext context) {
+    final CupertinoThemeData iOSTheme = CupertinoTheme.of(context);
+    final ThemeData androidTheme = Theme.of(context);
+
     return NativeShow(
       cupertinoChild: CupertinoButton(
         onPressed: onPressed,
         padding: padding,
         borderRadius: borderRadius,
         disabledColor: disabledColor!,
+        color: iOSTheme.primaryColor,
         child: child,
       ),
       materialChild: padding != null
           ? SizedBox(
               width: 169,
               height: 48,
-              child: TextButton(
+              child: ElevatedButton(
                 onPressed: onPressed,
-                style: TextButton.styleFrom(
-                    alignment: alignment,
-                    disabledForegroundColor: disabledColor,
-                    shape: RoundedRectangleBorder(borderRadius: borderRadius!)),
+                style: ElevatedButton.styleFrom(
+                  alignment: alignment,
+                  backgroundColor: androidTheme.primaryColor,
+                  disabledForegroundColor: disabledColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: borderRadius!,
+                  ),
+                ),
                 child: child,
               ),
             )
           : Padding(
               padding: padding!,
-              child: TextButton(
+              child: ElevatedButton(
                 onPressed: onPressed,
-                style: TextButton.styleFrom(
-                    alignment: alignment,
-                    disabledForegroundColor: disabledColor,
-                    shape: RoundedRectangleBorder(borderRadius: borderRadius!)),
+                style: ElevatedButton.styleFrom(
+                  alignment: alignment,
+                  backgroundColor: androidTheme.primaryColor,
+                  disabledForegroundColor: disabledColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: borderRadius!,
+                  ),
+                ),
                 child: child,
               ),
             ),
